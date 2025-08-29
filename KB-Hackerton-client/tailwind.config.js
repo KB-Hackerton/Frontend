01da2c9fe581dbaf1d22ee74d60e3374689722bf
kang-yesung <rkddptjd5228@gmail.com>
2025-08-29 13:51:36 +0900

    chore: tailwind css 색상 설정

--- KB-Hackerton-client/tailwind.config.js
+++ KB-Hackerton-client/tailwind.config.js
@@ -1,6 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Pretandard', 'sans-serif'],
+      },
+      colors: {
+        black: '#222222',
+        gray: {
+          100: '#F2F3F7',
+          200: '#BABABA',
+          300: '#7A7A7A',
+          400: '#3B3B3B',
+        },
+        white: '#FFFFFF',
+        blue: '#5EA7DF',
+        main: '#DA4C24',
+        pink: '#F58D80',
+        yellow: '#FFF600',
+        green: '#89B548',
+        red: '#FC0707',
+      },
+    },
+  },
   plugins: [],
 }
