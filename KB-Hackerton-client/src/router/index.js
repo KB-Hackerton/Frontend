28e4aaa07c4f04aa504c6ca4c549aecdf396d285
kang-yesung <rkddptjd5228@gmail.com>
2025-08-30 16:58:50 +0900

    Feature: 바텀 바로기기 페이지 router처리

--- KB-Hackerton-client/src/router/index.js
+++ KB-Hackerton-client/src/router/index.js
@@ -9,6 +9,26 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: () => import('@/views/CalendarView.vue'), // lazy
+    },
+    {
+      path: '/sos',
+      name: 'sos',
+      component: () => import('@/views/SosView.vue'), // lazy
+    },
+    {
+      path: '/chat',
+      name: 'chat-list',
+      component: () => import('@/views/ChatListView.vue'), // lazy
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: () => import('@/views/FavoritesView.vue'), // lazy
+    },
   ],
 })
 
