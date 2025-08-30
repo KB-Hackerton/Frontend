a1cac9a514801c71304237ffa0b496aa1f0bed5a
kang-yesung <rkddptjd5228@gmail.com>
2025-08-30 18:56:23 +0900

    Feature: 	알림 목록 페이지 추가 및 router 작성

--- KB-Hackerton-client/src/router/index.js
+++ KB-Hackerton-client/src/router/index.js
@@ -8,26 +8,49 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: '홈',
+      },
     },
     {
       path: '/calendar',
       name: 'calendar',
-      component: () => import('@/views/CalendarView.vue'), // lazy
+      component: () => import('@/views/CalendarView.vue'),
+      meta: {
+        title: '캘린더',
+      },
     },
     {
       path: '/sos',
       name: 'sos',
-      component: () => import('@/views/SosView.vue'), // lazy
+      component: () => import('@/views/SosView.vue'),
+      meta: {
+        title: 'SOS',
+      },
     },
     {
       path: '/chat-list',
       name: 'chat-list',
-      component: () => import('@/views/ChatListView.vue'), // lazy
+      component: () => import('@/views/ChatListView.vue'),
+      meta: {
+        title: '채팅 목록',
+      },
     },
     {
       path: '/favorites',
       name: 'favorites',
-      component: () => import('@/views/FavoritesView.vue'), // lazy
+      component: () => import('@/views/FavoritesView.vue'),
+      meta: {
+        title: '즐겨찾기',
+      },
+    },
+    {
+      path: '/notifications',
+      name: 'notifications',
+      component: () => import('@/views/NotificationsView.vue'),
+      meta: {
+        title: '알림',
+      },
     },
   ],
 })
---
+++ KB-Hackerton-client/src/views/NotificationsView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>알림목록</div>
+</template>
+
+<style scoped></style>
