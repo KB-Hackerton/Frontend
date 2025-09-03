55e54647467090c675c5a721e1e017649f1ae506
kang-yesung <rkddptjd5228@gmail.com>
2025-09-03 16:33:26 +0900

    Feature: 공고리스트 뷰 퍼블리싱

---
+++ KB-Hackerton-client/src/components/announce/SearchBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['search'])
+const searched = defineModel('searched')
+</script>
+
+<template>
+  <div class="flex gap-2 h-full w-full">
+    <div class="flex items-center border border-black rounded-[7px] h-[1.8rem] gap-2 flex-1">
+      <Icon icon="material-symbols:search-rounded" class="size-6 ml-2" />
+      <input
+        type="text"
+        placeholder="검색어를 입력해주세요"
+        v-model.trim="searched"
+        class="w-full rounded-xl h-full pl-[2px] text-14 medium"
+        style="outline: none; border: none; box-shadow: none"
+        @keyup.enter="emit('search')"
+      />
+      <Icon
+        v-if="searched"
+        icon="material-symbols:close-rounded"
+        class="size-6 mr-2"
+        @click="((searched = ''), emit('search'))"
+      />
+    </div>
+    <button
+      class="h-[1.8rem] w-[2.5rem] border border-black rounded-[7px] text-14 memdium"
+      @click="emit('search')"
+    >
+      검색
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
