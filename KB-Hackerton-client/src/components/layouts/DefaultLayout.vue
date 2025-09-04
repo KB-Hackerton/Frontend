f0c9e132eb737d3c466368e7b5bf00b3fa4a94a0
kang-yesung <rkddptjd5228@gmail.com>
2025-09-04 21:18:05 +0900

    Chore: scrollbar hidden 처리

--- KB-Hackerton-client/src/components/layouts/DefaultLayout.vue
+++ KB-Hackerton-client/src/components/layouts/DefaultLayout.vue
@@ -4,7 +4,9 @@ import { RouterView } from 'vue-router'
 
 <template>
   <div class="w-full flex-1 min-h-0 flex flex-col overflow-hidden">
-    <div class="w-full flex-1 min-h-0 overflow-y-auto px-4 pt-4 pb-[34px]">
+    <div
+      class="w-full flex-1 min-h-0 overflow-y-auto [&::-webkit-scrollbar]:hidden px-4 pt-4 pb-[34px]"
+    >
       <RouterView />
     </div>
   </div>
