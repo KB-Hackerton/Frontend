6a70165a533d26f495f555583a46ad691af50655
kang-yesung <rkddptjd5228@gmail.com>
2025-09-04 14:45:56 +0900

    Feature: 축제 상세 router추가

--- KB-Hackerton-client/src/router/festival.js
+++ KB-Hackerton-client/src/router/festival.js
@@ -7,4 +7,12 @@ export default [
       title: '축제 리스트',
     },
   },
+  {
+    path: '/festival-detail/:festival_id',
+    name: 'festivalDetail',
+    component: () => import('@/views/FestivalDetailView.vue'),
+    meta: {
+      title: '축제',
+    },
+  },
 ]
