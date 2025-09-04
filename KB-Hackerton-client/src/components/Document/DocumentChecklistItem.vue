bbf540e37862de4a5c85d777da070a2aca10f522
kang-yesung <rkddptjd5228@gmail.com>
2025-09-05 02:37:59 +0900

    Feature: 공고 서류 체크 뷰 퍼블리싱

---
+++ KB-Hackerton-client/src/components/Document/DocumentChecklistItem.vue
@@ -0,0 +1,15 @@
+<script setup>
+const checked = defineModel({ type: Boolean, default: false })
+const props = defineProps({
+  label: { type: String, required: true },
+})
+</script>
+
+<template>
+  <label class="flex gap-4 text-12 medium items-center">
+    <input type="checkbox" v-model="checked" />
+    <p>{{ props.label }}</p>
+  </label>
+</template>
+
+<style scoped></style>
