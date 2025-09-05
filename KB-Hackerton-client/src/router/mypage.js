81a8d7ac0aa8bdc9d9f94b630e19dc6d1cd306ff
snowman <rhun00moon@gmail.com>
2025-09-06 03:13:00 +0900

    Feature: 사업 정보 수정 페이지 router 추가

--- KB-Hackerton-client/src/router/mypage.js
+++ KB-Hackerton-client/src/router/mypage.js
@@ -3,8 +3,12 @@ export default [
     path: '/mypage',
     name: 'mypage',
     component: () => import('@/views/MyPageView.vue'),
-    meta: {
-      title: '마이페이지',
-    },
+    meta: { title: '마이페이지' },
+  },
+  {
+    path: '/mypage/business-edit',
+    name: 'business-edit',
+    component: () => import('@/views/edit/BusinessEditView.vue'),
+    meta: { title: '사업정보 수정' },
   },
 ]
