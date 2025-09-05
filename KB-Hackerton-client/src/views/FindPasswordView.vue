04ee14976f5ef2a600113b24b2ebbfa852e32712
snowman <rhun00moon@gmail.com>
2025-09-06 02:51:31 +0900

    Feature: 비밀번호 찾기 뷰 퍼블리싱

--- KB-Hackerton-client/src/views/FindPasswordView.vue
+++ KB-Hackerton-client/src/views/FindPasswordView.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import FindPasswordStepEmail from '@/components/find-password/FindPasswordStepEmail.vue'
+import FindPasswordStepReset from '@/components/find-password/FindPasswordStepReset.vue'
+
+const step = ref(1)
+
+function goNext() {
+  if (step.value < 2) step.value++
+}
+</script>
 
 <template>
-  <div class="w-full text-center">비밀번호찾기</div>
+  <div>
+    <FindPasswordStepEmail v-if="step === 1" @success="goNext" />
+    <FindPasswordStepReset v-else />
+  </div>
 </template>
-
-<style scoped></style>
