dea93ea9528130208b14ade05a4664399d9b5657
snowman <rhun00moon@gmail.com>
2025-09-06 00:25:00 +0900

    Feature: sos 리스트 menu 바 겹침 해결

--- KB-Hackerton-client/src/views/SosView.vue
+++ KB-Hackerton-client/src/views/SosView.vue
@@ -58,7 +58,7 @@ function handleEdit(item) {
     </div>
 
     <div
-      class="absolute bottom-0 left-0 right-0 max-h-[50%] min-h-[50%] bg-white rounded-t-2xl p-4 z-[999] overflow-y-auto"
+      class="absolute bottom-0 left-0 right-0 max-h-[50%] min-h-[50%] bg-white rounded-t-2xl p-4 z-[9] overflow-y-auto"
     >
       <template v-if="selectedItem">
         <SosDetail :item="selectedItem" :isOwner="true" @close="closeDetail" @edit="handleEdit" />
@@ -72,7 +72,7 @@ function handleEdit(item) {
     <button
       v-if="!selectedItem"
       @click="goToCreate"
-      class="absolute bottom-[52%] right-4 rounded-2xl px-4 py-2 bg-main text-white text-18 z-[1000]"
+      class="absolute bottom-[52%] right-4 rounded-2xl px-4 py-2 bg-main text-white text-18 z-[10]"
     >
       +
     </button>
