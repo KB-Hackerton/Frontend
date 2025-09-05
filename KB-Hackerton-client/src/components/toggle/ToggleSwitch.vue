c7ee68e01662a33bd69c0e48ab3230965e11aaef
kang-yesung <rkddptjd5228@gmail.com>
2025-09-05 14:37:16 +0900

    Feature: 토글 상태 props를 통해 데이터 바인딩 방법으로 변경

--- KB-Hackerton-client/src/components/toggle/ToggleSwitch.vue
+++ KB-Hackerton-client/src/components/toggle/ToggleSwitch.vue
@@ -1,14 +1,13 @@
 <script setup>
-const isClicked = defineModel({ isClicked: Boolean, default: false })
 const emits = defineEmits(['click'])
 
 const toggle = () => {
-  isClicked.value = !isClicked.value
   emits('click')
 }
 
 const props = defineProps({
-  label: String,
+  label: { type: String, required: false },
+  isChecked: { type: Boolean, required: true },
 })
 </script>
 
@@ -18,19 +17,19 @@ const props = defineProps({
       @click="toggle"
       :class="[
         'relative inline-flex h-5 w-12 items-center rounded-full transition-colors duration-300',
-        isClicked ? 'border border-2 border-main ' : ' border border-2 border-gray-300',
+        isChecked ? 'border border-2 border-main ' : ' border border-2 border-gray-300',
       ]"
     >
       <span
         :class="[
           'inline-block h-3 w-3 transform rounded-full  transition-transform duration-300',
-          isClicked ? 'translate-x-7 bg-main' : 'translate-x-1 bg-gray-300',
+          isChecked ? 'translate-x-7 bg-main' : 'translate-x-1 bg-gray-300',
         ]"
       />
     </button>
     <span
       v-if="props.label"
-      :class="['caption2 medium text-12', isClicked ? 'text-main' : 'text-gray-400']"
+      :class="['caption2 medium text-12', isChecked ? 'text-main' : 'text-gray-400']"
     >
       {{ props.label }}
     </span>
