86bb3f774313b5a820f9afdd98bf484303fa299a
snowman <rhun00moon@gmail.com>
2025-09-06 01:20:28 +0900

    Feature: 비밀번호 찾기 router 추가

--- KB-Hackerton-client/src/components/layouts/BottomNavigationBar.vue
+++ KB-Hackerton-client/src/components/layouts/BottomNavigationBar.vue
@@ -5,7 +5,7 @@ import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 const route = useRoute()
 
-const hiddenPrefixes = ['/login', '/signup', '/sos/create']
+const hiddenPrefixes = ['/login', '/signup', '/sos/create', '/find-password']
 
 const isHidden = computed(() => hiddenPrefixes.some((prefix) => route.path.startsWith(prefix)))
 </script>
---
+++ KB-Hackerton-client/src/views/FindPasswordView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="w-full text-center">비밀번호찾기</div>
+</template>
+
+<style scoped></style>
