b999873b79e5be83be2b792162345b2bf120a70e
kang-yesung <rkddptjd5228@gmail.com>
2025-09-08 21:41:50 +0900

    Chore: tailwind 색상 수정

--- KB-Hackerton-client/tailwind.config.js
+++ KB-Hackerton-client/tailwind.config.js
@@ -34,7 +34,8 @@ export default {
         ivory: '#FFF8EA',
         orange: {
           100: '#FF6900',
-          200: '#B43B1D',
+          200: '#DA4C24',
+          300: '#B43B1D',
         },
       },
       backgroundImage: {
